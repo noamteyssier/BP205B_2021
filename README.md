6f9381a80a6247f23bd918925170fa4e1a62fd70
noamteyssier <nb.teyssier@gmail.com>
2021-01-29 11:12:36 -0800

    added setup script

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # BP205B_2021
+
+# Installation
+
+```bash
+# setups git directory
+./setup.sh
+```
---
+++ setup.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+# initialize DeMultiSeq submodule
+git submodule init
+git submodule update
